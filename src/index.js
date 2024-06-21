658cd9283f37f70bb3d0156f9aa9cebeda2d581d
SOURYA DEY <souryadey031@gmail.com>
2024-06-21 12:13:56 +0530

    feat: Update index.js to include additional components and dependencies
    This commit updates the index.js file to include additional components and dependencies. It adds the BrowserRouter and Provider components from react-router-dom and react-redux respectively, to enable routing and state management in the application. It also imports and configures the store using the configureStore function from @reduxjs/toolkit, and sets up the Toaster component from react-hot-toast for displaying toast notifications. Additionally, it adds the ScrollToTop component and invokes the swDev function for service worker registration. These changes enhance the functionality and user experience of the application.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
+import { Toaster } from "react-hot-toast";
+import ScrollToTop from "./Components/ScrollToTop";
+import swDev from "./swDev";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+swDev();
