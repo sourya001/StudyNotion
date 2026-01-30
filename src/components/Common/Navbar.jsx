6365589e1420500c6e2df174e0eb8db23adef883
SOURYA DEY <souryadey031@gmail.com>
2026-01-30 20:17:50 +0530

    fixing catalog button

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -82,10 +82,10 @@ function Navbar() {
                     >
                       <p>{link.title}</p>
                       <BsChevronDown />
-                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
-                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg border border-richblack-600 bg-richblack-800 p-4 text-richblack-5 opacity-0 shadow-xl transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded border-l border-t border-richblack-600 bg-richblack-800"></div>
                         {loading ? (
-                          <p className="text-center">Loading...</p>
+                          <p className="text-center text-richblack-400">Loading...</p>
                         ) : subLinks?.length ? (
                           <>
                             {subLinks
@@ -98,7 +98,7 @@ function Navbar() {
                                     .split(" ")
                                     .join("-")
                                     .toLowerCase()}`}
-                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  className="rounded-lg py-3 pl-4 text-richblack-5 transition-colors hover:bg-richblack-700 hover:text-yellow-25"
                                   key={i}
                                 >
                                   <p>{subLink.name}</p>
@@ -106,7 +106,7 @@ function Navbar() {
                               ))}
                           </>
                         ) : (
-                          <p className="text-center">No Courses Found</p>
+                          <p className="text-center text-richblack-400">No Courses Found</p>
                         )}
                       </div>
                     </div>
