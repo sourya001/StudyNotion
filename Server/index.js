9616a6e731fc732eedaf9ded3650c06033c07fa3
SOURYA DEY <souryadey031@gmail.com>
2026-01-31 18:00:27 +0530

    ..

--- Server/index.js
+++ Server/index.js
@@ -60,9 +60,11 @@ app.get("/", (req, res) => {
 	});
 });
 
-// Listening to the server
-app.listen(PORT, () => {
-	console.log(`App is listening at ${PORT}`);
-});
+// Listening to the server (only when not on Vercel)
+if (process.env.VERCEL !== "1") {
+	app.listen(PORT, () => {
+		console.log(`App is listening at ${PORT}`);
+	});
+}
 
-// End of code.
+module.exports = app;
